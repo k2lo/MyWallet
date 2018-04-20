86cac159d0d1a7c367118261e43575e2fc7f3a9c
Karol Onaszkiewicz <karol.onaszkiewicz@radgost.com>
2018-04-20 02:01:00 +0200

    almost end

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,7 +7,7 @@ class Setting < ApplicationRecord
     def set_budget
       case self.currency
         when ISO4217::Currency.from_code('USD').symbol || nil
-            self.budget = self.budget
+            self.budget
         when ISO4217::Currency.from_code('EUR').symbol
             self.budget = self.budget * (ISO4217::Currency.from_code('EUR').exchange_rate)
         when ISO4217::Currency.from_code('PLN').symbol
