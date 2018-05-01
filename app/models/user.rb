be226938f82551aa14078f8cf548b54275ea1346
Karol Onaszkiewicz <karol.onaszkiewicz@radgost.com>
2018-05-01 17:39:35 +0200

    sign up fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   after_save :create_basic_settings
   
   def create_basic_settings
-    setting = Setting.new(:budget => 0, :currency => ISO4217::Currency.from_code('USD').symbol, :user_id => self.id)
+    setting = Setting.new(:budget => 0, :currency => 'USD', :user_id => self.id)
     setting.save
   end
 end
